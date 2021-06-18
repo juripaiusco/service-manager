a3842da6286aa6092d34d4514e896a2d27a480b3
Juri <juri@mr-j.it>
2021-06-18 20:34:24 +0200

    scadenze layout

--- v2/front-end/src/app/app.module.ts
+++ v2/front-end/src/app/app.module.ts
@@ -3,14 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { BreadcrumbComponent } from './header/breadcrumb/breadcrumb.component';
+
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ServiceComponent } from './views/service/service.component';
+import { CustomerComponent } from './views/customer/customer.component';
+import { HeaderComponent } from './header/header.component';
+import { ScadenzeComponent } from './views/dashboard/scadenze/scadenze.component';
+
+const routes: Routes = [{
+  path: 'dashboard',
+  component: DashboardComponent
+}, {
+  path: 'service',
+  component: ServiceComponent
+}, {
+  path: 'customer',
+  component: CustomerComponent
+}, {
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BreadcrumbComponent,
+    DashboardComponent,
+    ServiceComponent,
+    CustomerComponent,
+    HeaderComponent,
+    ScadenzeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ v2/front-end/src/app/views/dashboard/scadenze/scadenze.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { faSync, faAt, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-scadenze',
+  templateUrl: './scadenze.component.html',
+  styleUrls: ['./scadenze.component.css']
+})
+export class ScadenzeComponent implements OnInit {
+
+  faSync = faSync;
+  faAt = faAt;
+  faFileInvoiceDollar = faFileInvoiceDollar;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
