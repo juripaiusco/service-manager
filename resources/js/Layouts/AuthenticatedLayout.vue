841e02d9ae131e673d97889474cf5eb685af714c
Juri <juri@mr-j.it>
2023-07-12 17:59:13 +0200

    Init services

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,9 +29,27 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+
+                                <NavLink :href="route('service.index')" :active="route().current('service.index')">
+                                    Servizi
+                                </NavLink>
+
+<!--                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Clienti
+                                </NavLink>
+
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Fatturato
+                                </NavLink>
+
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Impostazioni
+                                </NavLink>-->
+
                             </div>
                         </div>
 
