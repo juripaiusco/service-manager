d3f382d19b32f506988d46dc74d7326e26f1fbe4
Juri <juri@mr-j.it>
2023-07-12 17:59:13 +0200

    Init services

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
+
+    Route::get('/service', [\App\Http\Controllers\Service::class, 'index'])->name('service.index');
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
