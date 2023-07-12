431256853fd48af762df336442b23ef9c2143b24
Juri <juri@mr-j.it>
2023-07-12 18:18:40 +0200

    Application Layout

---
+++ resources/js/Components/ApplicationHeader.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import Breadcrumb from "@/Components/Breadcrumb.vue";
+
+defineProps({
+    breadcrumbArray: Array,
+    textRight: String
+})
+
+</script>
+
+<template>
+
+    <div class="inline-flex w-full dark:text-white">
+        <div class="w-1/2">
+
+            <Breadcrumb :breadcrumb-array="breadcrumbArray" />
+
+        </div>
+        <div class="text-right w-1/2">
+
+            <slot name="textRight" />
+
+            {{ textRight }}
+
+        </div>
+    </div>
+</template>
