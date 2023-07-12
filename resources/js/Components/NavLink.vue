e45418b366b06e790bb34bf1b7dcc65e817c8fa3
Juri <juri@mr-j.it>
2023-07-12 22:10:02 +0200

    Fix dark text color

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 
 const classes = computed(() =>
     props.active
-        ? 'items-center px-1 pt-6 border-b-2 bg-sky-50 text-sky-700 border-sky-400 dark:border-sky-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-sky-700 transition duration-150 ease-in-out'
+        ? 'items-center px-1 pt-6 border-b-2 bg-sky-50 text-sky-700 border-sky-400 dark:border-sky-600 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-sky-700 transition duration-150 ease-in-out'
         : 'items-center px-1 pt-6 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
 );
 </script>
