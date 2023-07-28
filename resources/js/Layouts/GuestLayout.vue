dad8fe52d761b4c01d452153a4ac2c7289175dd4
Juri <juri@mr-j.it>
2023-07-28 08:59:52 +0200

    Login form space

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,12 +4,12 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col items-center pt-[100px] bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex flex-col items-center pt-[50px] bg-gray-100 dark:bg-gray-900">
         <div>
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500 m-auto" />
                 <br>
-                <h1 class="text-3xl dark:text-white">
+                <h1 class="text-3xl mb-[20px] dark:text-white">
                     Service<span class="font-bold">M</span> <small class="text-sm">v.2</small>
                 </h1>
             </Link>
