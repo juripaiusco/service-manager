91618d882f8263833824caee03838a91f29f1a85
Juri <juri@mr-j.it>
2023-08-07 23:44:58 +0200

    clear code

--- resources/js/Pages/Finance/Outcoming/Category.vue
+++ resources/js/Pages/Finance/Outcoming/Category.vue
@@ -5,10 +5,16 @@ import ApplicationHeader from "@/Components/ApplicationHeader.vue";
 import ApplicationContainer from "@/Components/ApplicationContainer.vue";
 import {__} from "@/ComponentsExt/Translations";
 import {__currency} from "@/ComponentsExt/Currency";
+import {__date} from "@/ComponentsExt/Date";
+import Table from "@/Components/Table/Table.vue";
+import {ref} from "vue";
 
 defineProps({
     data: Object,
+    filters: Object
 });
+
+const year_month_selected = ref(null);
 
 </script>
 
@@ -58,9 +64,9 @@ defineProps({
                 <thead>
                 <tr>
                     <th></th>
-                    <th v-for="(month, n) in data.months_list"
+                    <th v-for="(month, n) in data!.months_list"
                         :class="{
-                            'table-primary': data.today_month === n
+                            'table-primary': data!.today_month === n
                         }" >
                         {{ __(month) }}
                     </th>
@@ -68,27 +74,161 @@ defineProps({
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="(months_data, year) in data.category_count">
+                <tr v-for="(months_data, year) in data!.category_count">
                     <th class="text-left">
                         {{ year }}
                     </th>
                     <td v-for="(month_data, n) in months_data['m']"
                         class="text-right"
                         :class="{
-                            'table-primary': data.today_month === n
+                            'table-primary': data!.today_month === n
                         }" >
-                        {{ month_data ? __currency(month_data, 'EUR') : '-' }}
+                        <a :href="'#' + year + '-' + n"
+                           @click="year_month_selected=year + '-' + n">
+                            {{ month_data ? __currency(month_data, 'EUR') : '-' }}
+                        </a>
                     </td>
 
                     <th class="text-right">
-                        {{ __currency(months_data.total, 'EUR') }}
+                        {{ __currency(months_data!.total, 'EUR') }}
                     </th>
                 </tr>
                 </tbody>
             </table>
+
+            <template v-for="(invoices_data, year) in data!.invoices">
+
+                <br>
+
+                <div class="card">
+                    <div class="card-header">{{ year }}</div>
+                    <div class="card-body">
+
+                        <table class="table table-sm text-sm">
+                            <thead>
+                            <tr>
+                                <th class="text-left w-[140px]">
+                                    Numero
+                                </th>
+                                <th class="text-left">
+                                    Nome
+                                </th>
+                                <th class="text-center w-[80px]">
+                                    Tipo
+                                </th>
+                                <th class="text-center w-[80px]">
+                                    Data
+                                </th>
+                                <th class="text-right !align-top w-[100px]">
+                                    Importo
+                                </th>
+                                <th class="text-right !align-top w-[100px]">
+                                    IVA
+                                </th>
+                                <th class="text-right !align-top w-[100px]">
+                                    Totale
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="invoice in invoices_data"
+                                :id="year + '-' + __date(invoice.data, 'n')"
+                                :class="{
+                                    'year-month-selected': year_month_selected == year + '-' + __date(invoice.data, 'n')
+                                }">
+                                <td>
+                                    {{ invoice.numero }}
+                                </td>
+                                <td>
+                                    {{ invoice.nome }}
+                                </td>
+                                <td class="text-center">
+                                    {{ invoice.tipo_doc }}
+                                </td>
+                                <td class="text-center">
+                                    {{ __date(invoice.data, 'day') }}
+                                </td>
+                                <td class="text-right">
+                                    {{ __currency(invoice.importo_netto, 'EUR') }}
+                                </td>
+                                <td class="text-right">
+                                    {{ __currency(invoice.importo_iva, 'EUR') }}
+                                </td>
+                                <td class="text-right">
+                                    {{ __currency(invoice.importo_totale, 'EUR') }}
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <!-- <Table class="table-sm table-striped text-sm"
+                               :data="{
+                                filters: filters,
+                                tblName: 'invoice',
+                                routeSearch: 'finance.outcoming',
+                                data: invoices_data,
+                                structure: [{
+                                    class: 'text-left w-[140px]',
+                                    label: 'Numero',
+                                    field: 'numero',
+                                }, {
+                                    class: 'text-left',
+                                    label: 'Nome',
+                                    field: 'nome',
+                                }, {
+                                    class: 'text-left w-[40px]',
+                                    label: 'Tipo',
+                                    field: 'tipo_doc',
+                                }, {
+                                    class: 'text-center w-[40px]',
+                                    label: 'Data',
+                                    field: 'data',
+                                    fnc: function (d) {
+                                        return __date(d.data, 'day');
+                                    }
+                                }, {
+                                    class: 'text-right !align-top w-[100px]',
+                                    label: 'Importo',
+                                    field: 'importo_netto',
+                                    fnc: function (d) {
+
+                                        return d.importo_netto > 0 ? __currency(d.importo_netto, 'EUR') : '-'
+
+                                    }
+                                }, {
+                                    class: 'text-right !align-top w-[100px]',
+                                    label: 'IVA',
+                                    field: 'importo_iva',
+                                    fnc: function (d) {
+
+                                        return d.importo_iva > 0 ? __currency(d.importo_iva, 'EUR') : '-'
+
+                                    }
+                                }, {
+                                    class: 'text-right !align-top w-[100px]',
+                                    label: 'Totale',
+                                    field: 'importo_totale',
+                                    fnc: function (d) {
+
+                                        return d.importo_totale > 0 ? __currency(d.importo_totale, 'EUR') : '-'
+
+                                    }
+                                }]
+                    }" /> -->
+
+                    </div>
+                </div>
+
+            </template>
 
         </ApplicationContainer>
 
     </AuthenticatedLayout>
 
 </template>
+
+<style>
+.year-month-selected td {
+    background-color: lightblue !important;
+}
+</style>
