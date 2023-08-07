968d8a87998b37dd6e6b4da7b1d1914eff99f849
Juri <juri@mr-j.it>
2023-08-07 10:57:40 +0200

    close btn

--- resources/js/Pages/Finance/Outcoming/Category.vue
+++ resources/js/Pages/Finance/Outcoming/Category.vue
@@ -26,6 +26,28 @@ defineProps({
 
         <ApplicationContainer>
 
+            <div class="inline-flex w-full mb-6">
+
+                <div class="w-3/4">
+
+                    <Link class="btn btn-light"
+                          :href="route('finance.outcoming')"
+                          preserve-state
+                          preserve-scroll >
+
+                        <svg class="w-6 h-6"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+
+                    </Link>
+
+                </div>
+
+                <div class="w-1/4"></div>
+
+            </div>
+
             <table class="table table-sm text-sm">
                 <thead>
                 <tr>
